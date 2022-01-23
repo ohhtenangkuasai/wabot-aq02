93a724531afa2cc494aa7786dbc449fccbe9d33c
JulianKastaraaaa09 <97493187+JulianKastaraaaa09@users.noreply.github.com>
2022-01-23 10:43:15 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,8 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6289647417373', 'Bintangp02', m)
+  this.sendContact(m.chat, '1(507)4485743', 'SixxTenz(Owner Bot).', m)
+  this.sendContact(m.chat, '6281244706456', 'Kimiko(Admin Bot).', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
